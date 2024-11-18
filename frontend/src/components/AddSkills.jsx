c2415cbf46b2b36d4c257ef594d168715028cd89
ShreeshailChitpur <sjchitpur@gmail.com>
2024-11-18 12:39:15 +0530

    updates signup for skills - (_x_)

--- frontend/src/components/AddSkills.jsx
+++ frontend/src/components/AddSkills.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { availableSkills } from "../data/skills";
 
 const AddSkills = ({ user }) => {
   const [skill, setSkill] = useState("");
@@ -7,57 +8,6 @@ const AddSkills = ({ user }) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
-  // Sample list of skills
-  const availableSkills = [
-    "JavaScript",
-    "React",
-    "Node.js",
-    "CSS",
-    "Python",
-    "Java",
-    "C++",
-    "C#",
-    "PHP",
-    "Ruby",
-    "SQL",
-    "MongoDB",
-    "MySQL",
-    "PostgreSQL",
-    "HTML",
-    "TypeScript",
-    "Angular",
-    "Vue.js",
-    "Flutter",
-    "Kotlin",
-    "Swift",
-    "Dart",
-    "AWS",
-    "Azure",
-    "Google Cloud Platform",
-    "Docker",
-    "Kubernetes",
-    "Machine Learning",
-    "Data Science",
-    "Artificial Intelligence",
-    "DevOps",
-    "UI/UX Design",
-    "Product Design",
-    "Game Development",
-    "Cybersecurity",
-    "Blockchain",
-    "Data Engineering",
-    "Data Analysis",
-    "Web Development",
-    "Mobile Development",
-    "Backend Development",
-    "Frontend Development",
-    "Full Stack Development",
-    "Cloud Computing",
-    "Network Security",
-    "Ethical Hacking",
-    "Software Testing",
-    "Project Management",
-  ];
 
   useEffect(() => {
     if (user?.skills) {
